b9d9bbfedf3cc0135c25a15600d386ddfd0f5a93
caio <cgfuentelima0277@gmail.com>
2024-03-28 20:09:56 -0300

    criando classe abstrata

---
+++ src/classe_abstrata1/Pessoa.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classe_abstrata1;
+
+/**
+ *
+ * @author caio.gflima
+ */
+public class Pessoa {
+    
+}
